aedbc2fa1a32b4dac9ac6d7b7910dc4490f15a63
LuvianBella <1812210827@qq.com>
2018-11-27 08:55:22 +0800

    项目初始架构

---
+++ src/main/java/com/luvian/spring/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.luvian.spring;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spring1127Application.class);
+	}
+
+}
---
+++ src/main/java/com/luvian/spring/Spring1127Application.java
@@ -0,0 +1,12 @@
+package com.luvian.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1127Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1127Application.class, args);
+	}
+}
